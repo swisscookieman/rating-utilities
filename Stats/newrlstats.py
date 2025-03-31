125fd8157e674af0926068830b82caa17d46e507
swisscookieman <94302796+swisscookieman@users.noreply.github.com>
2025-03-31 16:09:36 +0200

    asda

--- Stats/newrlstats.py
+++ Stats/newrlstats.py
@@ -57,5 +57,5 @@ def process_file(input_file, output_file):
     with open(output_file, 'w') as wr:
         wr.writelines(lines)
 if True:
-    filename = "Stats/major1.csv"
+    filename = "Stats/major1correct.csv"
     process_file(filename, "Stats/processed-"+filename[6:])
