2fa82c60de36fb7c748edebee4517e33eb382282
swisscookieman <94302796+swisscookieman@users.noreply.github.com>
2025-03-31 08:28:34 +0200

    qwqw

--- Stats/newrlstats.py
+++ Stats/newrlstats.py
@@ -57,5 +57,5 @@ def process_file(input_file, output_file):
     with open(output_file, 'w') as wr:
         wr.writelines(lines)
 if True:
-    filename = "Stats/north-america-dxx4sk6tc3-players.csv"
+    filename = "Stats/major1.csv"
     process_file(filename, "Stats/processed-"+filename[6:])
